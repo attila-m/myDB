5836f34e1ffdd4b0f8d0be16eb1f0271ab1b0289
Attila Murvai <at.murvai@gmail.com>
2019-12-01 15:26:09 +0100

    add body-parsing to terminal

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
+const bodyParser = require('body-parser')
 
 const app = express();
 
+// pug
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+// routes
 app.use('/', routes);
+
 
 module.exports = app;
 
